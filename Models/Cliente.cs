356a8adf0d22e10ed7d990d0fd0f516e6b35d6de
Miguel Ángel Bermúdez Ramírez <migue.bermu93@gmail.com>
2019-09-17 02:00:28 +0200

    Inicio del proyecto copiar la página web de Agapea.com

---
+++ Models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agapea_MVC_NetCore.Models
+{
+    public class Cliente
+    {
+        #region "...propiedades de la clase..."
+
+        private string NIF;
+        private string NOMBRE;
+        private string APELLIDOS;
+        private string APELLIDOS;
+
+        #endregion
+        #region "...métodos de la clase..."
+        #region "...Constructores..."
+        #endregion
+        #endregion
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Agapea_MVC_NetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
