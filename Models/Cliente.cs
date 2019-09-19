5f32b824c608bded17c5603fde9592738997a6df
Miguel Ángel Bermúdez Ramírez <migue.bermu93@gmail.com>
2019-09-19 12:16:13 +0200

    Se han añadido clases y colecciones en general

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -7,25 +7,47 @@ namespace Agapea_MVC_NetCore.Models
 {
     public class Cliente
     {
+
         #region "...propiedades de la clase..."
 
-        public string sUsuario { get; set; }
-        public string sEmail { get; set; }
-        public string sContrasena { get; set; }
-        public string sNombre { get; set; }
-        public string sApellidos { get; set; }
-        public int iIentificador { get; set; }
-        public int iTelefono { get; set; }
-        public String sOtrosDatos { get; set; }
-        public int iNumeroDeTarjeta { get; set; }
-        public DateTime dCaducidadTarjeta { get; set; }
+        private String _usuario;
+        private String _email;
+        private String _contrasena;
+        private String _nombre;
+        private String _apellidos;
+        private String _otrosDatos;
+        private int _identificador;
+        private int _telefono;
+        private int _numeroDeTarjeta;
+        private DateTime _caducidadTarjeta;
 
-        Dictionary<String, Direccion> dictDirecciones { get; set; } = new Dictionary<string, Direccion>();
+        public String usuario
+        {
+            get
+            {
+                return _usuario;
+            }
+            set
+            {
+                this._usuario = value;
+            }
+        }
 
-        // crear un dictionary para pedidos y una clase pedido
+        public String email { get { return _email; } set { this._email = value; } }
+        public String contrasena { get { return _contrasena; } set { this._contrasena = value; } }
+        public String nombre { get {return _nombre;} set{this._nombre = value; }}
+        public String apellidos { get {return _apellidos;} set{this._apellidos = value; }}
+        public int identificador { get {return _identificador;} set{this._identificador = value; }}
+        public int telefono { get {return _telefono;} set{this._telefono = value; }}
+        public String otrosDatos { get {return _otrosDatos;} set{this._otrosDatos = value; }}
+        public int numeroDeTarjeta { get {return _numeroDeTarjeta;} set{this._numeroDeTarjeta = value; }}
+        public DateTime caducidadTarjeta { get {return _caducidadTarjeta; } set{this._caducidadTarjeta = value; }}
 
-        // Crear un dictionary para teléfonos
+        Dictionary<String, Direccion> direcciones { get; set; } = new Dictionary<string, Direccion>();
 
+        Dictionary<String, Pedido> pedidos { get; set; } = new Dictionary<string, Pedido>();
+
+        Dictionary<String, String> telefonos { get; set; } = new Dictionary<string, string>();
 
         public void borrarDireccion(String tipo)
         {
